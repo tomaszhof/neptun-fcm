b063d4e629ea56b17866634cbf6829df54f8aff5
Tomasz Hoffmann <tomaszhof@gmail.com>
2018-10-07 21:07:17 +0200

    demo test th

--- src/main/java/pl/neptun/model/User.java
+++ src/main/java/pl/neptun/model/User.java
@@ -3,8 +3,10 @@ package pl.neptun.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="people")
 public class User {
 	
 	@Id
