02263e1c937482e3e5f1894fd736d3f13f55be6f
Krzysztof <czarneckikrzysztof666@gmail.com>
2018-12-10 12:31:43 +0100

    powrot do zmian przed Proba zamiany getSingleResult na getResultList()

--- src/main/java/pl/neptun/jpa/NeptunDAO.java
+++ src/main/java/pl/neptun/jpa/NeptunDAO.java
@@ -89,31 +89,4 @@ public class NeptunDAO {
 
 			return result;
 		}
-
-	public static <T> T findByCode2(Class<T> clazz, String code) {
-		CriteriaBuilder cb = NeptunJPA.em().getCriteriaBuilder();
-		CriteriaQuery<T> q = cb.createQuery(clazz);
-
-		Root<T> root = q.from(clazz);
-
-		Predicate srcEqPredicate = cb.equal(root.get("code"), code);
-		q.select(root)
-				.where(srcEqPredicate);
-		TypedQuery<T> query = NeptunJPA.em().createQuery(q);
-		T result = null;
-		try {
-			//result = query.getSingleResult();
-			List results = q1.getResultList();
-			if (!results.isEmpty())
-				result = results.get(0);
-			else
-			// is it a problem? -> log.
-		} catch (Exception e) {
-			log.debug("Not found " + clazz.getName() + " where code=" + code);
-			log.debug(e.getMessage());
-		}
-
-		return result;
-	}
-
 }
