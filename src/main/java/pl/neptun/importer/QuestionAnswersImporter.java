f1d205e8cf3a6c518ad6dfa8363720b6fcdfc1dc
Krzysztof <czarneckikrzysztof666@gmail.com>
2018-12-13 09:09:56 +0100

    test

--- src/main/java/pl/neptun/importer/QuestionAnswersImporter.java
+++ src/main/java/pl/neptun/importer/QuestionAnswersImporter.java
@@ -57,8 +57,8 @@ public class QuestionAnswersImporter extends FileImporter {
 					answersCodes = data[2];
 					if ((questionCode != null) && (answersCodes != null)) {
 						logger.debug("Question: " + questionCode + " Answers: " + answersCodes);
-						Question q = NeptunDAO.findByCode(Question.class, questionCode); //tu zwraca null
-						//logger.debug("test: " + q);
+						//Question q = NeptunDAO.findByCode(Question.class, questionCode); //tu zwraca null
+						logger.debug("test: " + NeptunDAO.findByCode(Question.class, questionCode));
 						q.setAnswersCodes(answersCodes);
 						logger.debug("before persist.");
 						em.merge(q);
