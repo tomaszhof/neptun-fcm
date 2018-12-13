771a6a72860b0b235e082212e75132abfdb827c5
Krzysztof <czarneckikrzysztof666@gmail.com>
2018-12-13 09:02:36 +0100

    test

--- src/main/java/pl/neptun/jpa/NeptunDAO.java
+++ src/main/java/pl/neptun/jpa/NeptunDAO.java
@@ -79,7 +79,7 @@ public class NeptunDAO {
 					.where(srcEqPredicate);
 			TypedQuery<T> query = NeptunJPA.em().createQuery(q);
 
-			T result = "";
+			T result = new T();
 			try {
 				result = query.getSingleResult();
 				log.debug("Result: " + result);
