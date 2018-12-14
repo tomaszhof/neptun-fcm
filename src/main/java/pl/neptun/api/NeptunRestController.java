da4f1f3044e516c5e8ffc6b551ddfdce9536cd50
Krzysztof <czarneckikrzysztof666@gmail.com>
2018-12-14 22:46:29 +0100

    test JSON

--- src/main/java/pl/neptun/api/NeptunRestController.java
+++ src/main/java/pl/neptun/api/NeptunRestController.java
@@ -1,13 +1,9 @@
 package pl.neptun.api;
 
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +12,7 @@ import pl.neptun.jpa.NeptunDAO;
 import pl.neptun.model.Answer;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Transactional
@@ -26,20 +22,14 @@ public class NeptunRestController {
     Logger logger = LoggerFactory.getLogger(QuestionAnswersImporter.class);
 
     @GetMapping(path = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> sayHello() {
+    public HashMap<String, Object> sayHello() {
+        HashMap<String, Object> map = new HashMap<>();
         //Get data from service layer into entityList.
         List<Answer> entityList = NeptunDAO.findAll(Answer.class);
 
-        List<JSONObject> entities = new ArrayList<JSONObject>();
-        for (Answer n : entityList) {
-            JSONObject entity = new JSONObject();
-            try {
-                entity.put("aa", "bb");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            entities.add(entity);
+        for(Answer ans : entityList){
+            map.put(ans.getCode(), ans.getText());
         }
-        return new ResponseEntity<Object>(entities, HttpStatus.OK);
+        return map;
     }
 }
