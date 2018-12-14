2f26a6bcd9638b72ff23de4b7c7b6b07076316a0
Krzysztof <czarneckikrzysztof666@gmail.com>
2018-12-14 21:01:10 +0100

    test

--- src/main/java/pl/neptun/api/NeptunRestController.java
+++ src/main/java/pl/neptun/api/NeptunRestController.java
@@ -9,27 +9,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class NeptunRestController {
-    @GetMapping(path = "/hello", produces= MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/hello", produces=MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> sayHello()
     {
         //Get data from service layer into entityList.
 
         List<JSONObject> entities = new ArrayList<JSONObject>();
+        Entity[] entityList = new Entity[5];
 
+        for (Entity n : entityList) {
             JSONObject entity = new JSONObject();
-        try {
-            entity.put("aa", "bb");
-        } catch (JSONException e) {
-            e.printStackTrace();
+            try {
+                entity.put("aa", "bb");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            entities.add(entity);
         }
-        entities.add(entity);
-
-        return new ResponseEntity<Object>(entities, HttpStatus.OK);
+        return new ResponseEntity<>(entities, HttpStatus.OK);
     }
 }
