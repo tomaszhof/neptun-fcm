cb3942a872984e79041d85ff98bbc23f361e3262
Krzysztof <czarneckikrzysztof666@gmail.com>
2018-12-14 22:01:56 +0100

    test JSON

--- src/main/java/pl/neptun/api/NeptunRestController.java
+++ src/main/java/pl/neptun/api/NeptunRestController.java
@@ -1,9 +1,13 @@
 package pl.neptun.api;
 
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +16,7 @@ import pl.neptun.jpa.NeptunDAO;
 import pl.neptun.model.Answer;
 
 import javax.transaction.Transactional;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 @Transactional
@@ -22,14 +26,21 @@ public class NeptunRestController {
     Logger logger = LoggerFactory.getLogger(QuestionAnswersImporter.class);
 
     @GetMapping(path = "/hello", produces=MediaType.APPLICATION_JSON_VALUE)
-    public String sayHello()
+    public ResponseEntity<Object> sayHello()
     {
-        HashMap<String, Object> map = new HashMap<>(); //hashowa mapa zwraca JSON'a
-        List<Answer> list = NeptunDAO.findAll(Answer.class);
-        for(Answer ans : list){
-            logger.debug(String.valueOf(ans));
-        }
+        //Get data from service layer into entityList.
+        List<Answer> entityList = NeptunDAO.findAll(Answer.class);
 
-        return list.toString();
+        List<JSONObject> entities = new ArrayList<JSONObject>();
+        for (Answer n : entityList) {
+            JSONObject entity = new JSONObject();
+            try {
+                entity.put("aa", "bb");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            entities.add(entity);
+        }
+        return new ResponseEntity<Object>(entities, HttpStatus.OK);
     }
 }
