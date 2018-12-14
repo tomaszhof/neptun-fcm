7b01d1c177d3f7efbfbc4af1ff016f994810d04e
Krzysztof <czarneckikrzysztof666@gmail.com>
2018-12-14 21:23:37 +0100

    test JSON

--- src/main/java/pl/neptun/api/NeptunRestController.java
+++ src/main/java/pl/neptun/api/NeptunRestController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.neptun.jpa.NeptunDAO;
 import pl.neptun.model.Answer;
 
 import java.util.HashMap;
@@ -16,28 +17,9 @@ public class NeptunRestController {
     @GetMapping(path = "/hello", produces=MediaType.APPLICATION_JSON_VALUE)
     public HashMap<String, Object> sayHello()
     {
+        HashMap<String, Object> map = new HashMap<>(); //hashowa mapa zwraca JSON'a
+        map = (HashMap<String, Object>) NeptunDAO.findAll(Answer.class);
 
-        /*
-        //Get data from service layer into entityList.
-        List<Entity> entityList = new ArrayList<>();
-
-        List<JSONObject> entities = new ArrayList<JSONObject>();
-
-
-        for (Entity n : entityList) {
-            JSONObject entity = new JSONObject();
-            try {
-                entity.put("aa", "bb");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            entities.add(entity);
-        }
-        */
-        //return new ResponseEntity<>(entities, HttpStatus.OK);
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("key1", "value1");
-        map.put("results", Answer.class);
         return map;
     }
 }
